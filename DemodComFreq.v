f1278ab2e4d7dd9d2a1dd7d45069d86099547c5b
Rafael Brier <rafaelmjf2010@gmail.com>
2018-05-28 22:16:48 -0300

    pequenas mudanças

---
+++ DemodComFreq.v
@@ -0,0 +1,66 @@
+module DemodComFreq
+#( 
+	parameter numberOfBitProtocolBegin = 4'd7
+)
+(
+	input clk,
+	input VppmIn,
+	input freqAvailable,
+	input [31:0] signalFrequency,
+	output reg dataRead	
+);
+
+reg initFlag;
+reg [24:0] cont;
+reg readPoint;
+reg protocolBegin;
+reg [3:0] protocolCont;
+
+
+initial begin
+initFlag = 1'b0;
+readPoint = 1'b0;
+cont = 25'd0;
+protocolCont = 4'b0;
+end
+
+//protocolBegin
+always @ (posedge VppmIn) begin
+if(protocolCont < numberOfBitProtocolBegin-1) begin
+protocolCont<= protocolCont + 1'b1;
+protocolBegin <= 1'b0;
+end
+else 
+protocolBegin <= 1'b1;
+end
+
+//Trigger
+always @ (*) begin
+if(protocolBegin && freqAvailable )
+initFlag <= 1'b1;
+end
+
+//Leitura
+always @ (posedge clk) begin
+if(initFlag) begin
+if(cont == 25'd0) begin
+dataRead <= ~VppmIn;
+readPoint <= 1'b1;
+end
+else
+readPoint <= 1'b0;
+end
+end
+
+
+//Contador
+always @ (posedge clk) begin
+if(initFlag) begin
+if(cont <= signalFrequency)
+cont <= cont + 1'b1;
+else
+cont <= 1'b0;
+end
+end
+endmodule
+
--- Mainn.v
+++ Mainn.v
@@ -1,3 +1,16 @@
+/*
+	-Sequência de pelo menos 5 bits 0 em VPPM: 00000;
+	para detecção da frequência.
+	-Inicio dos dados após bit 0:  0 101010101010
+											 ^ ^          ^	
+											 | |   dados  |
+											 |
+									   sincronia
+	-Demodula infinitamente apos isto
+	-Devo adicionar reset.
+*/
+
+
 module Mainn
 
 #(
@@ -9,13 +22,22 @@ module Mainn
 
 );
 
+parameter clockFreq = 32'd200000000;
+
 wire [NBITS:0] inCount;
 wire inputRead;
 wire clk_50, iCLK, iCLK_n, clk_200; 
 wire clk_defFreq;
 wire dataRead;
 wire [31:0] freqCalculada1, freqCalculada2, freqCalculada3, freqCalculada4;
-reg [31:0] vppmFrequencyDetected;
+wire [31:0] vppmFrequencyDetected;
+reg [31:0] freqParam;
+reg freqAvailable;
+
+initial begin
+freqAvailable = 1'b0;
+end
+
 
 //PLL----------------------------------------------------------------------------------------
 PLL32 PLL(clk, clk_50, iCLK, iCLK_n, clk_200);
@@ -42,12 +64,23 @@ freqDetect #(3'd3)freqDetector3(clk_200, inputRead, freqCalculada3);
 freqDetect #(3'd4)freqDetector4(clk_200, inputRead, freqCalculada4);
 //Calcular média (máximo 4 vezes, para menores colocar 0 no freqCalculada respectivo)
 Median     #(3'd4)calcMedia(clk_200, freqCalculada1, freqCalculada2, freqCalculada3,
-																							freqCalculada4);
+																							freqCalculada4,
+																							vppmFrequencyDetected);
 //---------------------------------------------------------------------------------------------
-
+always @ (posedge clk_200) begin
+if(vppmFrequencyDetected != 32'd0) begin
+freqParam <= clockFreq/vppmFrequencyDetected -2;
+freqAvailable <= 1'd1;
+end
+else begin
+freqParam <= 32'd10;
+freqAvailable <= 1'd0;
+end
+end
 
 //Demodulador (Parameter = 200MHz/SinalFreq - 2)(SinalFreq = AstavelFreq/2)--------------------------------------
-Demodulador DemodVPPM(clk_200, inputRead, 32'd38, dataRead);
+//Demodulador DemodVPPM(clk_200, inputRead, 32'd38, dataRead);
+DemodComFreq #(4'd7)DemodVPPMComFreq(clk_200, inputRead, freqAvailable, freqParam, dataRead);
 //---------------------------------------------------------------------------------------------
 
 endmodule
